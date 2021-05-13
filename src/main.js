d6c291019846897e44b9aa41807c50185b699023
yueshang-anran <3239255489@qq.com>
2021-05-13 19:01:50 +0800

    提交文件

--- src/main.js
+++ src/main.js
@@ -2,11 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+import axios from 'axios'
+import './axios.js' // 请求拦截
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import './permission.js' // 路由拦截
 
+// use
+
+Vue.use(mavonEditor)
+Vue.use(Element)
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
